327b18e14c410f4684ff61550e8a1ef19c7716d0
Samarendra M H <samarendra@osuosl.org>
2017-07-06 13:11:31 -0700

    Better way of pointing to right packer binary
    Paths differ on the build slaves.
    Also, functions are recommended over aliases in bash scripts.

--- build-image.sh
+++ build-image.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 CHEF_VERSION="provisionerless"
 TEMPLATE=
+PPC64LE_PACKER='~/bento/packer/bin/packer'
+X86_64_PACKER='/usr/local/bin/packer'
 
 run_help() {
   echo " Usage: $0 [-c] [-v CHEF_VERSION] -t path/to/template.json"
@@ -34,7 +36,17 @@ done
 
 [ -z "$TEMPLATE" ] && echo "Error: Template file not set" && exit 1
 # use the ppc64/le packer for the right one
-[[ $TEMPLATE == *'ppc'* ]] && alias packer='~/bento/packer/bin/packer'
+packer()
+{
+  if [[ $TEMPLATE == *'ppc'* ]]; then
+    echo "Using ppc64le packer for arguments $*"
+    $PPC64LE_PACKER $*
+  else
+    $X86_64_PACKER $*
+  fi
+}
+
+export -f packer
 
 DIR_NAME="packer-$(basename -s .json $TEMPLATE)"
 IMAGE_NAME=$(grep vm_name $TEMPLATE | awk '{print $2}' | sed -e 's/\"//g' | sed -e 's/,//g')
