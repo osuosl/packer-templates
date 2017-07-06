352a44ed047b33a2ff122128c3c177212ca579e2
Samarendra M H <samarendra@osuosl.org>
2017-07-06 11:05:21 -0700

    All the necessary scripts for centos-7.2ppc64

--- templates/scripts/opensuse/sudoers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -eux
-
-# update sudoers - can't do this in autoinst.xml
-echo -e "\nupdate sudoers ..."
-echo -e "opensuse ALL=(ALL) NOPASSWD: ALL\n" >> /etc/sudoers
--- templates/scripts/ubuntu/resolv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -eux
-
-echo $DNS_SERVER_1  >> /etc/resolvconf/resolv.conf.d/base
-echo $DNS_SERVER_2 >> /etc/resolvconf/resolv.conf.d/base
-resolvconf -u
