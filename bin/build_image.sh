303f825bd4f0b83d785f6f9e95103676e6b4277d
Samarendra M H <samarendra@osuosl.org>
2017-07-24 18:41:59 -0700

    enable packer logs at debug level from the build_image process

--- bin/build_image.sh
+++ bin/build_image.sh
@@ -39,12 +39,14 @@ DIR_NAME="packer-$(basename -s .json $TEMPLATE)"
 TEMPLATE_NAME="$(basename -s .json $TEMPLATE)"
 IMAGE_NAME=$(grep vm_name $TEMPLATE | awk '{print $2}' | sed -e 's/\"//g' | sed -e 's/,//g')
 set -xe
+
 if [ -e chef/${TEMPLATE_NAME}/Berksfile ] ; then
   export BERKSHELF_PATH="chef/berkshelf"
   rm -rf $BERKSHELF_PATH chef/${TEMPLATE_NAME}/cookbooks chef/${TEMPLATE_NAME}/Berksfile.lock
   berks vendor -b chef/${TEMPLATE_NAME}/Berksfile chef/${TEMPLATE_NAME}/cookbooks
 fi
-packer build -var "chef_version=$CHEF_VERSION" $(basename $TEMPLATE)
+
+PACKER_LOG=1 && packer build -debug -var "chef_version=$CHEF_VERSION" $(basename $TEMPLATE)
 
 if [ "$(packer version | grep ^Packer)" == "Packer v0.7.5" ] ; then
   qemu-img convert -o compat=0.10 -O qcow2 -c ${DIR_NAME}/${IMAGE_NAME}.qcow2 \
