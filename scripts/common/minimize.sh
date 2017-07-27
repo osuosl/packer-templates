6089631544e446a9a8cb03bcb17ac78483dd6284
Jonathan Frederick <doublej472@osuosl.org>
2017-07-27 14:29:35 -0700

    Dont fail on missing swap

--- scripts/common/minimize.sh
+++ scripts/common/minimize.sh
@@ -4,12 +4,12 @@
 # Swap is disabled till reboot 
 readonly swapuuid=$(/sbin/blkid -o value -l -s UUID -t TYPE=swap)
 readonly swappart=$(readlink -f /dev/disk/by-uuid/"$swapuuid")
-/sbin/swapoff "$swappart"
+/sbin/swapoff "$swappart" || echo "swapoff failed, ignoring"
 dd if=/dev/zero of="$swappart" bs=1M || echo "dd exit code $? is suppressed" 
-/sbin/mkswap -U "$swapuuid" "$swappart"
+/sbin/mkswap -U "$swapuuid" "$swappart" || echo "mkswap failed, ignoring"
 rm -rf /tmp/*
 
-dd if=/dev/zero of=/EMPTY bs=1M
+dd if=/dev/zero of=/EMPTY bs=1M || echo "dd exit code $? is suppressed" 
 rm -f /EMPTY
 # Block until the empty file has been removed, otherwise, Packer
 # will try to kill the box while the disk is still full and that's bad
