b18ca2975c9760272ebd25264389b6a4d74badd7
Samarendra M H <samarendra@osuosl.org>
2017-07-30 16:53:48 -0700

    if the qcow2 built image already exists, skip building it

--- bin/build_image.sh
+++ bin/build_image.sh
@@ -38,7 +38,14 @@ done
 DIR_NAME="packer-$(basename -s .json $TEMPLATE)"
 TEMPLATE_NAME="$(basename -s .json $TEMPLATE)"
 IMAGE_NAME=$(grep vm_name $TEMPLATE | awk '{print $2}' | sed -e 's/\"//g' | sed -e 's/,//g')
+FINAL_QCOW_FILE_NAME="${DIR_NAME}/${IMAGE_NAME}-compressed.qcow2"
+
 set -xe
+
+if [ -f $FINAL_QCOW_FILE_NAME ] ; then
+   echo "A built qcow2 already present for $IMAGE_NAME. Skipping!"
+   exit 1
+fi
 
 if [ -e chef/${TEMPLATE_NAME}/Berksfile ] ; then
   export BERKSHELF_PATH="chef/berkshelf"
@@ -48,10 +55,11 @@ fi
 
 PACKER_LOG=1 && packer build -debug -var "chef_version=$CHEF_VERSION" $(basename $TEMPLATE)
 
+
 if [ "$(packer version | grep ^Packer)" == "Packer v0.7.5" ] ; then
   qemu-img convert -o compat=0.10 -O qcow2 -c ${DIR_NAME}/${IMAGE_NAME}.qcow2 \
-    ${DIR_NAME}/${IMAGE_NAME}-compressed.qcow2
+    $FINAL_QCOW_FILE_NAME
 else
   qemu-img convert -o compat=0.10 -O qcow2 -c ${DIR_NAME}/${IMAGE_NAME} \
-    ${DIR_NAME}/${IMAGE_NAME}-compressed.qcow2
+    $FINAL_QCOW_FILE_NAME
 fi
