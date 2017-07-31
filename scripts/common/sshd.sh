e559201e7e28ec6490676d79645b9f62bb557858
Jonathan Frederick <doublej472@osuosl.org>
2017-07-31 10:37:55 -0700

    Make sshd script more generic

--- scripts/common/sshd.sh
+++ scripts/common/sshd.sh
@@ -1,8 +1,21 @@
 #!/bin/bash -eux
 
-echo "UseDNS no" >> /etc/ssh/sshd_config
-echo "PermitRootLogin no" >> /etc/ssh/sshd_config
-echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
-echo "KbdInteractiveAuthentication no" >> /etc/ssh/sshd_config
-sed -i 's/PasswordAuthentication .*/PasswordAuthentication no/Ig' /etc/ssh/sshd_config
-sed -i 's/ChallengeResponseAuthentication .*/ChallengeResponseAuthentication no/Ig' /etc/ssh/sshd_config
+grep -q "^UseDNS" /etc/ssh/sshd_config\
+  && sed -i 's/UseDNS .*/UseDNS no/Ig' /etc/ssh/sshd_config\
+  || echo "UseDNS no" >> /etc/ssh/sshd_config;
+
+grep -q "^PermitRootLogin" /etc/ssh/sshd_config\
+  && sed -i 's/PermitRootLogin .*/PermitRootLogin no/Ig' /etc/ssh/sshd_config\
+  || echo "PermitRootLogin no" >> /etc/ssh/sshd_config;
+
+grep -q "^GSSAPIAuthentication" /etc/ssh/sshd_config\
+  && sed -i 's/GSSAPIAuthentication .*/GSSAPIAuthentication no/Ig' /etc/ssh/sshd_config\
+  || echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config;
+
+grep -q "^KbdInteractiveAuthentication" /etc/ssh/sshd_config\
+  && sed -i 's/KbdInteractiveAuthentication .*/KbdInteractiveAuthentication no/Ig' /etc/ssh/sshd_config\
+  || echo "KbdInteractiveAuthentication no" >> /etc/ssh/sshd_config;
+
+grep -q "^ChallengeResponseAuthentication" /etc/ssh/sshd_config\
+  && sed -i 's/ChallengeResponseAuthentication .*/ChallengeResponseAuthentication no/Ig' /etc/ssh/sshd_config\
+  || echo "ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config;
