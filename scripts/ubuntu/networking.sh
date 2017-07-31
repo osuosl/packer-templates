3ce328f41574134d85f3a16cfa328c4aab488a9a
Jonathan Frederick <doublej472@osuosl.org>
2017-07-31 15:47:20 -0700

    Fix boot

--- scripts/ubuntu/networking.sh
+++ scripts/ubuntu/networking.sh
@@ -4,6 +4,10 @@ echo "Disabling automatic udev rules for network interfaces in Ubuntu"
 # Disable automatic udev rules for network interfaces in Ubuntu,
 # appends cmdline options to the linux kernel
 sed -i 's/append\=\"/append\=\"net\.ifnames\=0\ /' /etc/yaboot.conf
+  || echo "Yaboot not found!"
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT\=\"/GRUB_CMDLINE_LINUX_DEFAULT\=\"net\.ifnames\=0 /' /etc/default/grub
+  && grub-mkconfig -o /boot/grub/grub.cfg
+  || echo "Grub2 not found!"
 
 # Fix ethernet address for network interfaces
 sed -i 's/auto\ en.*/auto\ eth0/' /etc/network/interfaces
