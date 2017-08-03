7c4317ebfd22359d46f94b2cef206acec64eb0d7
Jonathan Frederick <doublej472@osuosl.org>
2017-08-03 10:02:56 -0700

    We aren't using vagrant

--- scripts/ubuntu/sudoers.sh
+++ scripts/ubuntu/sudoers.sh
@@ -2,6 +2,6 @@
 
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers;
 
-# Set up password-less sudo for the vagrant user
-echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >/etc/sudoers.d/99_vagrant;
-chmod 440 /etc/sudoers.d/99_vagrant;
+# Set up password-less sudo for the ubuntu user
+echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' >/etc/sudoers.d/99_ubuntu;
+chmod 440 /etc/sudoers.d/99_ubuntu;
