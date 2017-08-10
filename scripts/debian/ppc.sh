ecd99b9c5664d10ed2a569e1a3ed2116bfc8cee4
Jonathan Frederick <doublej472@osuosl.org>
2017-08-10 16:19:41 -0700

    Force ppc64-diag installation on debian

--- scripts/debian/ppc.sh
+++ scripts/debian/ppc.sh
@@ -4,4 +4,4 @@ mkdir -p /etc/apt/sources.list.d
 echo 'deb "http://packages.osuosl.org/repositories/apt/" jessie main' > /etc/apt/sources.list.d/osuosl.list
 
 apt-get update
-apt-get install -y ppc64-diag
+apt-get install --force-yes -y ppc64-diag
