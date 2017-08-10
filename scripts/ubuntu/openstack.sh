b7ef4ab4cdaa2eace96f87d28532bfbe313cbaca
Jonathan Frederick <doublej472@osuosl.org>
2017-08-10 10:37:47 -0700

    Fix typo

--- scripts/ubuntu/openstack.sh
+++ scripts/ubuntu/openstack.sh
@@ -1,7 +1,7 @@
 apt-get -y install pump cloud-utils cloud-init cloud-initramfs-growroot \
     bash-completion
 
-# Ignore wardning about datasources
+# Ignore warning about datasources
 mkdir -p /etc/cloud/cloud.cfg.d
 cat <<EOF >/etc/cloud/cloud.cfg.d/99-warnings.cfg;
 #cloud-config
