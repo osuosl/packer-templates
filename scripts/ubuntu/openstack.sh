9d7c314d829674681a0c63ad18b7a7f9c1a35e48
Jonathan Frederick <doublej472@osuosl.org>
2017-08-11 15:52:15 -0700

    Work around datasource warning in ubuntu

--- scripts/ubuntu/openstack.sh
+++ scripts/ubuntu/openstack.sh
@@ -1,10 +1,14 @@
 apt-get -y install pump cloud-utils cloud-init cloud-initramfs-growroot \
     bash-completion
 
-# Ignore warning about datasources
-mkdir -p /etc/cloud/cloud.cfg.d
-cat <<EOF >/etc/cloud/cloud.cfg.d/99-warnings.cfg;
-#cloud-config
-warnings:
-  dsid_missing_source: off
+# Speed up cloud-init by only using Ec2 and a specific metadata url
+cat >> /etc/cloud/cloud.cfg.d/91_Ec2_override.cfg << EOF
+
+# Force only Ec2 being enabled
+datasource_list: ['Ec2']
+datasource:
+  Ec2:
+    metadata_urls: [ 'http://169.254.169.254' ]
+    timeout: 5 # (defaults to 50 seconds)
+    max_wait: 10 # (defaults to 120 seconds)
 EOF
