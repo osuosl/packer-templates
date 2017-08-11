067f87bb5c90f3b2ad9fb87a5f05fb289699d975
Jonathan Frederick <doublej472@osuosl.org>
2017-08-11 11:18:20 -0700

    Don't ignore datasource warnings in debian

--- scripts/debian/openstack.sh
+++ scripts/debian/openstack.sh
@@ -1,10 +1,2 @@
 apt-get -y install pump cloud-utils cloud-init cloud-initramfs-growroot \
     bash-completion
-
-# Ignore wardning about datasources
-mkdir -p /etc/cloud/cloud.cfg.d
-cat <<EOF >/etc/cloud/cloud.cfg.d/99-warnings.cfg;
-#cloud-config
-warnings:
-  dsid_missing_source: off
-EOF
