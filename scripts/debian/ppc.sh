12e39103259f01b30a224ef2374a513ce264ab76
Jonathan Frederick <doublej472@osuosl.org>
2017-08-16 14:02:41 -0700

    Use proper gpg key checking

--- scripts/debian/ppc.sh
+++ scripts/debian/ppc.sh
@@ -3,5 +3,7 @@
 mkdir -p /etc/apt/sources.list.d
 echo 'deb "http://packages.osuosl.org/repositories/apt/" jessie main' > /etc/apt/sources.list.d/osuosl.list
 
+wget -qO - http://packages.osuosl.org/repositories/apt/repo.gpg | sudo apt-key add -
+
 apt-get update
-apt-get install --force-yes -y ppc64-diag
+apt-get install -y ppc64-diag
