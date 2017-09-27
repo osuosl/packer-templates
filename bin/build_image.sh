a8a989a5311d51a2c60163fff5bd016e4cd9c2e3
Samarendra M H <samarendra@osuosl.org>
2017-09-27 16:34:58 -0700

    clean berks vendor dirs before vendoring new cookbooks
    This should prevents any remnant bugs

--- bin/build_image.sh
+++ bin/build_image.sh
@@ -43,7 +43,7 @@ fi
 if [ -e chef/${TEMPLATE_NAME}/Berksfile ] ; then
   export BERKSHELF_PATH="chef/berkshelf"
   rm -rf $BERKSHELF_PATH chef/${TEMPLATE_NAME}/cookbooks chef/${TEMPLATE_NAME}/Berksfile.lock
-  berks vendor -b chef/${TEMPLATE_NAME}/Berksfile chef/${TEMPLATE_NAME}/cookbooks
+  berks vendor --delete -b chef/${TEMPLATE_NAME}/Berksfile chef/${TEMPLATE_NAME}/cookbooks
 fi
 export PACKER_LOG=1
 packer build -color=false -force -debug $(basename $TEMPLATE)
