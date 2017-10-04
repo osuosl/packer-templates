fa73e7c442fce1aab0343e7ddad6ff718205bc86
Samarendra M H <samarendra@osuosl.org>
2017-10-04 14:47:31 -0700

    Removing Ubuntu related files which are not used by any other templates

--- scripts/ubuntu/ppc.sh
+++
@@ -1,1 +0,0 @@
-apt-get -y install ppc64-diag
--- scripts/ubuntu/sudoers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers;
-
-# Set up password-less sudo for the ubuntu user
-echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' >/etc/sudoers.d/99_ubuntu;
-chmod 440 /etc/sudoers.d/99_ubuntu;
