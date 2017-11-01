4bdf3d23631e228b7c70136bee72fd3f4063afd2
Cody Holliday <codysseus@osuosl.org>
2017-11-01 16:51:53 -0700

    Deletes all old images for the PR. Fixed deploy_wrapper parameter passing.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -59,7 +59,13 @@ done
 
 if [ "$PUBLISH" == 0 ]; then
     if [ ! -r "$FILE" ]; then echo "Error: Cannot read file '$FILE'. Try '$0 -h'" && exit 1; fi
-    openstack image delete "$IMG_NAME - PR#$PR_NUM"
+
+    old_image="$IMG_NAME - PR#$PR_NUM"
+    uuids=`openstack image list | grep $old_image | sed "s/| //" | awk '{print $1}'`
+    for id in $uuids; do
+      openstack image delete $id;
+    done
+
     openstack image create --file "$FILE" --property chef-version="$CHEF_VER" --disk-format qcow2 "$IMG_NAME - PR#$PR_NUM"
     exit $?
 else
