fa036789aea221bb35ae217f77ca4be4588adc49
Lance Albertson <lance@osuosl.org>
2017-12-19 16:13:03 -0800

    Remove services and packages we no longer need
    This supersedes #45.

--- scripts/fedora/cleanup.sh
+++ scripts/fedora/cleanup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -eux
 if [ -x /usr/bin/dnf ] ; then
   echo "using dnf"
-  dnf -y remove gcc cpp kernel-devel kernel-headers
+  dnf -y remove gcc cpp kernel-devel kernel-headers rpcbind postfix
   dnf -y clean all
 else
   echo "using yum"
-  yum -y remove gcc cpp kernel-devel kernel-headers
+  yum -y remove gcc cpp kernel-devel kernel-headers rpcbind postfix
   yum -y clean all
 fi
+
+echo "port 0" >> /etc/chrony.conf
+echo "cmdport 0" >> /etc/chrony.conf
+
 rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
 rm -f /tmp/chef*rpm
