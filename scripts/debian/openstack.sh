4dc798b344df5a1f712fc63f20e02adab315c2a2
Lance Albertson <lance@osuosl.org>
2018-07-28 09:35:12 -0700

    Set hostname in cloud-init so sudo doesn't complain

--- scripts/debian/openstack.sh
+++ scripts/debian/openstack.sh
@@ -1,2 +1,7 @@
 #!/bin/bash -eux
 apt-get -y install pump cloud-utils cloud-init cloud-initramfs-growroot bash-completion
+
+cat >> /etc/cloud/cloud.cfg.d/91_openstack_override.cfg << EOF
+# Set the hostname in /etc/hosts so sudo doesn't complain
+manage_etc_hosts: true
+EOF
