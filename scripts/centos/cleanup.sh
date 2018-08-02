b97ec6e607954c2e847a82eaa1bdc7bab31cada7
Lance Albertson <lance@osuosl.org>
2018-08-02 11:40:31 -0700

    Don't create a directory called /etc/udev/rules.d/70-persistent-net.rules
    This breaks cloud-init :(

--- scripts/centos/cleanup.sh
+++ scripts/centos/cleanup.sh
@@ -10,7 +10,6 @@ rm -f /tmp/chef*rpm
 
 # Clean up network interface persistence
 rm -f /etc/udev/rules.d/70-persistent-net.rules;
-mkdir -p /etc/udev/rules.d/70-persistent-net.rules;
 rm -f /lib/udev/rules.d/75-persistent-net-generator.rules;
 rm -rf /dev/.udev/;
 
