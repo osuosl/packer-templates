eff66700b99cdeb067a99798167ab2e1fb4df5d2
Lance Albertson <lance@osuosl.org>
2019-04-18 16:04:20 -0700

    Enable cloud-init services, remove default netplan config file

--- scripts/ubuntu/ganeti.sh
+++ scripts/ubuntu/ganeti.sh
@@ -1,5 +1,7 @@
 apt-get -y install pump cloud-utils cloud-init cloud-initramfs-growroot \
     bash-completion
+systemctl enable cloud-init cloud-init-local
+rm -f /etc/netplan/01-netcfg.yaml
 # No timeout for grub menu
 sed -i -e 's/^GRUB_TIMEOUT.*/GRUB_TIMEOUT=0/' /etc/default/grub
 # # Write out the config
