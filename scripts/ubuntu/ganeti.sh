09961dab4c7b3a2fd95eddff74b29b6629650d77
Lance Albertson <lance@osuosl.org>
2019-04-25 13:08:20 -0700

    Don't remove default netcfg and also just use network.sh for the grub settings

--- scripts/ubuntu/ganeti.sh
+++ scripts/ubuntu/ganeti.sh
@@ -1,8 +1,3 @@
 apt-get -y install pump cloud-utils cloud-init cloud-initramfs-growroot \
     bash-completion
 systemctl enable cloud-init cloud-init-local
-rm -f /etc/netplan/01-netcfg.yaml
-# No timeout for grub menu
-sed -i -e 's/^GRUB_TIMEOUT.*/GRUB_TIMEOUT=0/' /etc/default/grub
-# # Write out the config
-update-grub
