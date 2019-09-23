b8f1e3dfe9f2d43093321fec3db3c90c2f8f9de3
Jonathan Frederick <doublej472@osuosl.org>
2019-09-23 15:30:59 -0700

    Remove Debian 8

--- scripts/debian/ppc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eux
-
-mkdir -p /etc/apt/sources.list.d
-echo 'deb "http://packages.osuosl.org/repositories/apt/" jessie main' > /etc/apt/sources.list.d/osuosl.list
-
-wget -qO - http://packages.osuosl.org/repositories/apt/repo.gpg | sudo apt-key add -
-
-apt-get update
-apt-get install -y ppc64-diag
