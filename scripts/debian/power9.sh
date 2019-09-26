fb83954be8ac67a020ac482a1a6eef6d78e453d2
Lance Albertson <lance@osuosl.org>
2019-09-26 16:45:35 -0700

    Use oldstable now that buster is out for this

--- scripts/debian/power9.sh
+++ scripts/debian/power9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
-echo 'APT::Default-Release "stable";' > /etc/apt/apt.conf.d/99defaultrelease
+echo 'APT::Default-Release "oldstable";' > /etc/apt/apt.conf.d/99defaultrelease
 
 cat > /etc/apt/sources.list.d/testing.list <<EOF
 deb http://deb.debian.org/debian testing main contrib non-free
