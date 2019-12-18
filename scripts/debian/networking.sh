bb41367295ec81deff7af2acf2dc2719db632f55
Lance Albertson <lance@osuosl.org>
2019-12-18 09:22:54 -0800

    Debian 10 aarch64

--- scripts/debian/networking.sh
+++ scripts/debian/networking.sh
@@ -19,8 +19,6 @@ fi
 if [ "$major_version" -ge "9" ]; then
   # Disable Predictable Network Interface names and use eth0
   sed -i 's/en[[:alnum:]]*/eth0/g' /etc/network/interfaces;
-  sed -ie 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0 \1"/g' /etc/default/grub;
-  update-grub;
 fi
 
 # Adding a 2 sec delay to the interface up, to make the dhclient happy
--- scripts/debian/openstack.sh
+++ scripts/debian/openstack.sh
@@ -2,6 +2,25 @@
 apt-get -y install cloud-utils cloud-init cloud-initramfs-growroot \
   bash-completion parted
 
+if [ -e /etc/default/grub ] ; then
+  # output bootup to serial
+  sed -i -e \
+    's/GRUB_CMDLINE_LINUX=\"\(.*\)/GRUB_CMDLINE_LINUX=\"console=ttyS0,115200n8 console=tty0 \1/g' \
+    /etc/default/grub
+  # No timeout for grub menu
+  sed -i -e 's/^GRUB_TIMEOUT.*/GRUB_TIMEOUT=0/' /etc/default/grub
+  # No fancy boot screen
+  grep -q rhgb /etc/default/grub && sed -e 's/rhgb //' /etc/default/grub
+  # Debian doesn't create BOOTAA64.EFI by default, so do it manually so the VM
+  # can boot properly.
+  if [[ ! -e /boot/efi/EFI/BOOT/BOOTAA64.EFI && "$(uname -m)" == "aarch64" ]] ; then
+    mkdir -p /boot/efi/EFI/BOOT
+    cp /boot/efi/EFI/debian/grubaa64.efi /boot/efi/EFI/BOOT/BOOTAA64.EFI
+  fi
+  # Write out the config
+  update-grub
+fi
+
 cat >> /etc/cloud/cloud.cfg.d/91_openstack_override.cfg << EOF
 # Set the hostname in /etc/hosts so sudo doesn't complain
 manage_etc_hosts: true
