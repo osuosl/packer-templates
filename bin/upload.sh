ece91be4948a6d2ffe7e2a4ae74b547fd768cda8
Lance Albertson <lance@osuosl.org>
2020-03-21 21:52:58 -0700

    Add upload helper script

---
+++ bin/upload.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+run_help() {
+  echo
+  echo " NAME:"
+  echo "    $0 - Upload target image to OpenStack"
+  echo
+  echo " USAGE:"
+  echo "    $0 [-v | -f [FILE] -n [IMG_NAME]"
+  echo
+  echo " OPTIONS:"
+  echo "    -v          - Show verbose output."
+  echo
+  echo " ARGUMENTS:"
+  echo "    FILE        - Fully built image to be uploaded to OpenStack if [-p] is not"
+  echo "                  specified. The image name will be: '\$IMG_NAME -PR#\$PR_NUM'."
+  echo
+  echo "    IMG_NAME    - Name of the new image or name of the image to replace if"
+  echo "                  [-p] is specified."
+	echo
+  echo "    OPTIONS     - Raw options for the image. i.e. '--property <key1=value> --property <key2=value>'"
+  exit 0
+}
+
+while getopts "hpvf:c:n:r:d:o:" opt ; do
+  case ${opt} in
+    h)
+      run_help
+      ;;
+    v)
+      set -x
+      ;;
+    f)
+      FILE="$OPTARG"
+      ;;
+    n)
+      IMG_NAME="$OPTARG"
+      ;;
+    o)
+      OPTIONS="$OPTARG"
+      ;;
+  esac
+done
+
+[ -z "$1" ] && run_help
+[ -z "$IMG_NAME" ] && echo "Error: IMG_NAME not set. Try '$0 -h'" && exit 1
+
+openstack image create \
+  --file "$FILE" \
+  --disk-format raw \
+  --property hw_scsi_model=virtio-scsi \
+  --property hw_disk_bus=scsi \
+  --property hw_qemu_guest_agent=yes \
+  --property os_require_quiesce=yes \
+  $OPTIONS \
+  "$IMG_NAME"
