630e7cd74470fdea6282e3e68d6e86e845bf38ee
Lance Albertson <lance@osuosl.org>
2023-01-25 11:44:45 -0800

    Remove yamllint which is only used for testing
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- scripts/common/remove-cinc.sh
+++ scripts/common/remove-cinc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 if [ -f "/bin/dnf" ] ; then
-  dnf remove -y cinc
+  dnf remove -y cinc yamllint
 elif [ -f "/usr/bin/apt-get" ] ; then
-  apt-get -y purge cinc
+  apt-get -y purge cinc yamllint
 fi
 rm -rf /tmp/cinc
