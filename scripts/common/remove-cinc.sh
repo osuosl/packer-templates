a14daac81477cda78ad0e65cf9d1d695bf647174
Lance Albertson <lance@osuosl.org>
2023-10-02 17:35:41 -0700

    Update to support EL7
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- scripts/common/remove-cinc.sh
+++ scripts/common/remove-cinc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -eux
 if [ -f "/bin/dnf" ] ; then
   dnf remove -y cinc
+elif [ -f "/bin/yum" ] ; then
+  yum remove -y cinc
 elif [ -f "/usr/bin/apt-get" ] ; then
   apt-get -y purge cinc
 fi
