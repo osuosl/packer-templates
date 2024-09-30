046a10290da7ddba8255ff68581b09303dcb231e
Lance Albertson <lance@osuosl.org>
2024-09-30 13:10:27 -0700

    fixes
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- scripts/opensuse/zypper-locks.sh
+++ scripts/opensuse/zypper-locks.sh
@@ -1,4 +1,12 @@
 #!/bin/sh -eux
 
+# Wait until the system is fully booted
+while ! systemctl is-system-running --quiet; do
+  echo "Waiting for the system to fully boot..."
+  sleep 1
+done
+
+echo "System has fully booted. Proceeding with further commands..."
+
 # remove zypper locks on removed packages to avoid later dependency problems
 zypper --non-interactive rl \*
