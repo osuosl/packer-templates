040fcd3e958b601dba7a36c46aaf5eca863a14ee
Lance Albertson <lance@osuosl.org>
2024-09-30 10:05:46 -0700

    Add OpenSUSE 15.6 ppc64le
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- scripts/opensuse/cleanup.sh
+++ scripts/opensuse/cleanup.sh
@@ -7,5 +7,15 @@ zypper -n rm -u gcc make kernel-default-devel kernel-devel
 rm -f /etc/udev/rules.d/70-persistent-net.rules;
 touch /etc/udev/rules.d/75-persistent-net-generator.rules;
 
+rm -f /var/lib/wicked/lease*
+
 # delete any logs that have built up during the install
 find /var/log/ -name *.log -exec rm -f {} \;
+
+truncate -s 0 /etc/machine-id
+if test -f /var/lib/dbus/machine-id
+then
+  truncate -s 0 /var/lib/dbus/machine-id  # if not symlinked to "/etc/machine-id"
+fi
+
+m -f /var/lib/systemd/random-seed
