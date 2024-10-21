6f4f29ce522aa57bb7b8d7abccd8b176b0c50f93
Lance Albertson <lance@osuosl.org>
2024-10-21 14:20:36 -0700

    Docker template for use with multi-node testing
    This will replace the ancient CentOS Atomic image.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

---
+++ scripts/centos/docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -eux
+dnf install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl enable docker
