ecd9a20a6f5b3666c6866978a0c02e7870fe7754
Lance Albertson <lance@osuosl.org>
2025-05-29 09:28:42 -0700

    AlmaLinux 10
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- scripts/centos/docker.sh
+++ scripts/centos/docker.sh
@@ -2,4 +2,10 @@
 dnf install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+cat <<EOF > /etc/docker/daemon.json
+{
+  "experimental": true,
+  "live-restore": true
+}
+EOF
 systemctl enable docker
