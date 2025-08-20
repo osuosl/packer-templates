e3a62696ff33de20ce3a72ad3a3e2e5c71de996d
Lance Albertson <lance@osuosl.org>
2025-08-20 11:27:37 -0700

    Remove Windows 2012r2
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- scripts/windows/compact.bat
+++
@@ -1,4 +0,0 @@
-net stop wuauserv
-rmdir /S /Q C:\Windows\SoftwareDistribution\Download
-mkdir C:\Windows\SoftwareDistribution\Download
-net start wuauserv
--- scripts/windows/install_chocolatey.ps1
+++
@@ -1,1 +0,0 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
